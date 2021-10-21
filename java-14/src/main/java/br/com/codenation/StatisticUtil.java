e5e6cacd6d38bc74a9aa00d559e4f4e4bb76f38b
Lucas André <lucas.cancado89@gmail.com>
2021-10-20 22:00:25 -0300

    DEsafio 2 done

--- java-14/src/main/java/br/com/codenation/StatisticUtil.java
+++ java-14/src/main/java/br/com/codenation/StatisticUtil.java
@@ -1,13 +1,31 @@
 package br.com.codenation;
 
+import java.util.Arrays;
+
 public class StatisticUtil {
 
 	public static int average(int[] elements) {
-		return 0;
+		int totalSum = Arrays.stream(elements).sum();
+		return totalSum / elements.length;
 	}
 
 	public static int mode(int[] elements) {
-		return 0;
+		int theMoreRepeats = 0;
+		int repeats = 0;
+		int moda = 0;
+		for (int n: elements) {
+			for (int num: elements) {
+				if (n == num) {
+					repeats++;
+				}
+			}
+			if (repeats > theMoreRepeats) {
+				theMoreRepeats = repeats;
+				moda = n;
+			}
+			repeats = 0;
+			}
+		return moda;
 	}
 
 	public static int median(int[] elements) {
